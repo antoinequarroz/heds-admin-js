222dbe95430706d4d71a7dc12b4e7e4bfbc97c62
AntoineQuarroz <antoine.quarroz@hes-so.ch>
2023-06-21 15:52:31 +0200

    Commit de l'après-miudi continuatgion de préscédant commit

--- src/components/MaterialList.vue
+++ src/components/MaterialList.vue
@@ -1,13 +1,60 @@
 <template>
-  <div>
-    <!-- Mettez votre code HTML pour la liste de matériaux ici -->
+  <!-- omitting other part for brevity -->
+  <div class="tab-pane fade show active" id="course-pills-tabs-1" role="tabpanel" aria-labelledby="course-pills-tab-1">
+    <div class="row g-4">
+      <!-- Card item START -->
+      <div class="col-sm-6 col-lg-4 col-xl-3" v-for="course in courses" :key="course.id">
+        <div class="card shadow h-100">
+          <!-- Image -->
+          <img :src="course.image" class="card-img-top" alt="course image">
+          <!-- Card body -->
+          <div class="card-body pb-0">
+            <!-- Badge and favorite -->
+            <div class="d-flex justify-content-between mb-2">
+              <a href="#" class="badge bg-purple bg-opacity-10 text-purple">All level</a>
+              <a href="#" class="h6 mb-0"><i class="far fa-heart"></i></a>
+            </div>
+            <!-- Title -->
+            <h5 class="card-title fw-normal"><a href="#">{{ course.title }}</a></h5>
+            <p class="mb-2 text-truncate-2">{{ course.description }}</p>
+            <!-- Rating star -->
+            <ul class="list-inline mb-0">
+              <!-- Your rating logic here -->
+            </ul>
+          </div>
+          <!-- Card footer -->
+          <div class="card-footer pt-0 pb-3">
+            <hr>
+            <div class="d-flex justify-content-between">
+              <span class="h6 fw-light mb-0"><i class="far fa-clock text-danger me-2"></i>{{ course.duration }}</span>
+              <span class="h6 fw-light mb-0"><i class="fas fa-table text-orange me-2"></i>{{ course.lectures }} lectures</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Card item END -->
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'; // Make sure you have installed axios with `npm install axios`
+
 export default {
   name: "MaterialList",
-  // Ajoutez votre logique Vue.js ici si nécessaire
+  data() {
+    return {
+      courses: []
+    }
+  },
+  async created() {
+    try {
+      const response = await axios.get('https://your-api-endpoint/courses');
+      this.courses = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
 </script>
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,11 +18,51 @@
               <div class="navbar-nav">
                 <a class="nav-link active" :href="homePath">Accueil</a>
                 <a class="nav-link active" :href="materialPath">Matériels</a>
-                <a class="nav-link active" href="scenarioPath">Scénario</a>
-                <a class="nav-link active" href="eventPath">Calendrier</a>
+                <a class="nav-link active" :href="scenarioPath">Scénario</a>
+                <a class="nav-link active" :href="eventPath">Calendrier</a>
                 <a class="nav-link active" :href="adminPath">Admin</a>
               </div>
             </div>
+
+            <!-- Nav Search START -->
+            <div class="nav my-3 my-xl-0 px-4 flex-nowrap align-items-center">
+              <div class="nav-item w-100">
+                <form class="position-relative">
+                  <input class="form-control pe-5 bg-transparent" type="search" placeholder="Recherche" aria-label="Recherche">
+                  <button class="bg-transparent p-2 position-absolute top-50 end-0 translate-middle-y border-0 text-primary-hover text-reset" type="submit">
+                    <i class="fas fa-search fs-6 "></i>
+                  </button>
+                </form>
+              </div>
+            </div>
+            <!-- Nav Search END -->
+
+            <!-- Profile START -->
+            <div class="dropdown ms-1 ms-lg-0">
+              <a class="avatar avatar-sm p-0" href="#" id="profileDropdown" role="button" data-bs-auto-close="outside" data-bs-display="static" data-bs-toggle="dropdown" aria-expanded="false">
+                <img class="avatar-img rounded-circle" src="src/assets/images/avatar/01.jpg" alt="avatar">
+              </a>
+              <ul class="dropdown-menu dropdown-animation dropdown-menu-end shadow pt-3" aria-labelledby="profileDropdown">
+                <!-- Profile info -->
+                <li class="px-3 mb-3">
+                  <div class="d-flex align-items-center">
+                    <!-- Avatar -->
+                    <div class="avatar me-3">
+                      <img class="avatar-img rounded-circle shadow" src="src/assets/images/avatar/01.jpg" alt="avatar">
+                    </div>
+                    <div>
+                      <a class="h6" href="#">Votre Nom</a>
+                      <p class="small m-0">example@hes-so.ch</p>
+                    </div>
+                  </div>
+                </li>
+                <li> <hr class="dropdown-divider"></li>
+                <!-- Links -->
+                <li><a class="dropdown-item" href="#"><i class="bi bi-person fa-fw me-2"></i>Edit Profile</a></li>
+                <li><a class="dropdown-item bg-danger-soft-hover" href="#"><i class="bi bi-power fa-fw me-2"></i>Sign Out</a></li>
+              </ul>
+            </div>
+            <!-- Profile START -->
           </div>
         </nav>
       </div>
